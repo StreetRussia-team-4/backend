62a68f1dd7e7dbb92b0a73229a6c9b805b33fbe6
Crafty Plonkton <craftyplonkton@gmail.com>
2024-05-09 08:03:44 +0300

    infrastructure, User model, swagger

---
+++ cnfg/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+]
