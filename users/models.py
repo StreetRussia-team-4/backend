08088c2d917652a2f77edd4dd13fbd9b80ae19e1
Crafty Plonkton <craftyplonkton@gmail.com>
2024-05-12 00:49:35 +0300

    feat: add status and login through email to user molel

--- users/models.py
+++ users/models.py
@@ -1,7 +1,23 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
+    FEDERAL_MANAGER = 'FEDM'
+    REGIONAL_MANAGER = 'REGM'
+    USER = 'USER'
+    STATUSES = (
+        (FEDERAL_MANAGER, 'Федеральный руководитель'),
+        (REGIONAL_MANAGER, 'Региональный руководитель'),
+        (USER, 'Пользователь')
+    )
+    username = None
+    status = models.CharField('Статус', max_length=4, choices=STATUSES, default=USER)
+    email = models.EmailField('Электронная почта', unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
