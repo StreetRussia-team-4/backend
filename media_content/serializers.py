78341e730fb2890325e32c387ac7f3ad49d8ff9a
Секачев Максим <sekachev@rt-bpm.ru>
2024-05-27 08:39:11 +0300

    link for ImageSerializer VideoSerializer

--- media_content/serializers.py
+++ media_content/serializers.py
@@ -1,3 +1,4 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from media_content.models import Image, Video
@@ -5,15 +6,18 @@ from media_content.models import Image, Video
 
 class ImageSerializer(ModelSerializer):
     """Serializer для модели Изображение."""
+    link = serializers.ImageField(source='image')
 
     class Meta:
         model = Image
-        fields = ('id', 'image',)
+        fields = ('id', 'link',)
 
 
 class VideoSerializer(ModelSerializer):
     """Serializer для модели Видео."""
 
+    link = serializers.ImageField(source='video')
+
     class Meta:
         model = Video
-        fields = ('id', 'video',)
+        fields = ('id', 'link',)
